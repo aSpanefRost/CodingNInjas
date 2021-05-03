4169f27eeeee4f2beb1cbbf837633ee6e105a48a
Rajdeep Das <58761773+aSpanefRost@users.noreply.github.com>
2021-05-03 13:12:01 +0530

    Create Replace_Character_Recursively.cpp

---
+++ Advanced Recursion/Replace_Character_Recursively.cpp
@@ -0,0 +1,34 @@
+// Replace Character Recursively
+// Send Feedback
+// Given an input string S and two characters c1 and c2, you need to replace every occurrence of character c1 with character c2 in the given string.
+// Do this recursively.
+// Input Format :
+// Line 1 : Input String S
+// Line 2 : Character c1 and c2 (separated by space)
+// Output Format :
+// Updated string
+// Constraints :
+// 1 <= Length of String S <= 10^6
+// Sample Input :
+// abacd
+// a x
+// Sample Output :
+// xbxcd
+
+void replaceCharacter(char input[], char c1, char c2) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * No need to print or return the output. 
+     * Change in the given input string itself.
+     * Taking input and printing output is handled automatically.
+     */
+     for(int i=0;;i++)
+     {
+         if(input[i]=='\n') break;
+         else
+         {
+             if(input[i]==c1) input[i]=c2;
+         }
+     }
+    
+}
