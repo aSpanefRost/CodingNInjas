0993f114e6ef8db7a35a8cc4a9c4cbca24a5f8bd
Rajdeep Das <58761773+aSpanefRost@users.noreply.github.com>
2021-05-03 13:13:47 +0530

    Create Merge_Sort_Code.cpp

---
+++ Advanced Recursion/Merge_Sort_Code.cpp
@@ -0,0 +1,93 @@
+// Merge Sort Code
+// Send Feedback
+// Sort an array A using Merge Sort.
+// Change in the input array itself. So no need to return or print anything.
+// Input format :
+// Line 1 : Integer n i.e. Array size
+// Line 2 : Array elements (separated by space)
+// Output format :
+// Array elements in increasing order (separated by space)
+// Constraints :
+// 1 <= n <= 10^3
+// Sample Input 1 :
+// 6 
+// 2 6 8 5 4 3
+// Sample Output 1 :
+// 2 3 4 5 6 8
+// Sample Input 2 :
+// 5
+// 2 1 5 2 3
+// Sample Output 2 :
+// 1 2 2 3 5 
+
+
+void merge(int arr[], int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    // Create temp arrays
+    int L[n1], R[n2];
+ 
+    // Copy data to temp arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    // Merge the temp arrays back into arr[l..r]
+ 
+    // Initial index of first subarray
+    int i = 0;
+ 
+    // Initial index of second subarray
+    int j = 0;
+ 
+    // Initial index of merged subarray
+    int k = l;
+ 
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted */
+void MergeSort(int arr[],int l,int r){
+    if(l>=r){
+        return;//returns recursively
+    }
+    int m = (l+r-1)/2;
+    MergeSort(arr,l,m);
+    MergeSort(arr,m+1,r);
+    merge(arr,l,m,r);
+}
+void mergeSort(int input[], int size){
+	// Write your code here
+      MergeSort(input,0,size-1);  
+}
